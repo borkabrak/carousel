e5d28358ee9bc13b7d3e49f7b8d0b25603d41a4f
Jonathan Carter <jcarter@str.com>
2014-01-28 16:56:00 -0600

    Visually distinguish selected menu element.

--- carousel.js
+++ carousel.js
@@ -4,15 +4,16 @@ var Carousel = function(items){
     var me = this;
     me.paper = new Raphael("carousel", 800, 300);
     me.paper.showRuler(50);
+
     me.items = items;
+
+    me.selected = 0;
 
     me.draw();
 
 };
 
 Carousel.prototype = {
-    selected: function(){
-    },
 
     select: function(index){
         console.log("Select menu item %s", index);
@@ -30,22 +31,38 @@ Carousel.prototype = {
         var height = me.paper.height;
         var width = me.paper.width / me.items.length;
         var x = 0;
+
+        // draw menu items
         me.items.forEach(function(item){
 
-            var frame = me.paper.rect(x, 0, width, height).attr({
-                "stroke": "black",
-                "stroke-width": "3px",
-            });
+            var attributes = (me.items.indexOf(item) === me.selected) ? {
+                frame: {
+                    "fill"          : "#335", 
+                    "stroke"        : "black",
+                    "stroke-width"  : "3px",
+                },
+
+                text: {
+                    "fill": "white",
+                },
+
+            } : {
+                frame: {
+                },
+                text: {
+                },
+            };
+
+            var frame = me.paper.rect(x, 0, width, height).attr(attributes.frame);
 
             var middle_of_frame = { 
                 x: frame.attr("x") + (frame.attr("width")  / 2), 
                 y: frame.attr("y") + (frame.attr("height") / 2), 
             };
 
-            var text = me.paper.text(middle_of_frame.x, middle_of_frame.y, item.name);
+            var text = me.paper.text(middle_of_frame.x, middle_of_frame.y, item.name).attr(attributes.text);
 
             x += frame.attr("width");
-            console.log("%s >> frame:%o (middle:%o), text:%o, next:%s", item.name, frame, middle_of_frame, text, x);
         });
     },
 };
